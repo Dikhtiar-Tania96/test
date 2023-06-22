7619e84f12294759477ca9ca591ea4e4cfd23084
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2023-06-22 21:59:43 +0300

    add new file

---
+++ js/index.js
@@ -0,0 +1,80 @@
+// // урок 1
+// // Піднесення до степеня
+// let base = prompt('Введіть число');
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt('Давай степінь');
+// power = Number(power);
+// console.log(power)
+
+// let result = base ** power;
+// console.log(result)
+
+// const salary = 100;
+// if(salary > 500){
+//     console.log('Урааа')
+// }if (console.log('Uuups'));
+
+
+// // switch
+// const option = 13;
+// let message = '';
+
+// switch(option){
+//     case 1:
+//     message = 'Ви можете забрати товар завтра з 12:00 у нашому магазині'
+//     break;
+
+//     case 2:
+//     message = 'Курєр доставить ваш товар';
+//     break;
+
+//     case 3:
+//     message = 'Посилка буде відправлення сьогодні';
+//     break;
+
+//     default:
+//         message = 'Ми вам зателефонуємо';
+// }
+// console.log(message)
+
+// цикл for
+// for (let i = 10; i => 6; i -= 1){
+// console.log(i)
+
+// }
+// console.log('sssss')
+
+// Задача 1
+// const minSalary = 500;
+// const maxSalary = 5000;
+// const employees = 4;
+// let totalSalary = 0;
+
+// for(let i = 1; i <= employees; i += 1){
+//     const salary = Math.round(
+//         Math.random() * (maxSalary - minSalary) + minSalary
+//     );
+//     console.log(`ЗП робітника номер ${i} - ${salary}`);
+//     totalSalary += salary; 
+// };
+
+// console.log(`totalSalary: `, totalSalary);
+
+// задача 
+const min = 6;
+const max = 13;
+let total = 0;
+
+for (let i = min; i <= max; i += 1){
+    console.log(i);
+
+    if(i % 2 === 0){
+        console.log('парне:' , i);
+    }
+
+    if(i % 2 !== 0){
+        console.log(`не парне:`, i);
+    }
+}

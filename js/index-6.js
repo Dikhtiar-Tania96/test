b5b70f2c5007bb6b4e3872ce36986f3db1501d6d
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-02-02 14:25:43 +0200

    add new file

--- js/index-6.js
+++ js/index-6.js
@@ -87,55 +87,55 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-const cars = [
-    {
-      id:1,
-      model: "Honda",
-      type: "Civic",
-      price: 12000,
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
-    },
-    {
-      id:2,
-      model: "Audi",
-      type: "Q7",
-      price: 40000,
-      img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
-    },
-    {
-      id:3,
-      model: "BMW",
-      type: "5 siries",
-      price: 9000,
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
-    },
-    {
-      id:4,
-      model: "Honda",
-      type: "Accord",
-      price: 20000,
-      img: "https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg",
-    },
-    {
-        id:5,
-        model: "Honda",
-        type: "Civic",
-        price: 12000,
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
-      },
-      {
-        model: "Audi",
-        type: "Q7",
-        price: 40000,
-        img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
-      },
-      {
-        model: "BMW",
-        type: "5 siries",
-        price: 9000,
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
-      }
-];
+// const cars = [
+//     {
+//       id:1,
+//       model: "Honda",
+//       type: "Civic",
+//       price: 12000,
+//       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
+//     },
+//     {
+//       id:2,
+//       model: "Audi",
+//       type: "Q7",
+//       price: 40000,
+//       img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
+//     },
+//     {
+//       id:3,
+//       model: "BMW",
+//       type: "5 siries",
+//       price: 9000,
+//       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
+//     },
+//     {
+//       id:4,
+//       model: "Honda",
+//       type: "Accord",
+//       price: 20000,
+//       img: "https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg",
+//     },
+//     {
+//         id:5,
+//         model: "Honda",
+//         type: "Civic",
+//         price: 12000,
+//         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
+//       },
+//       {
+//         model: "Audi",
+//         type: "Q7",
+//         price: 40000,
+//         img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
+//       },
+//       {
+//         model: "BMW",
+//         type: "5 siries",
+//         price: 9000,
+//         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
+//       }
+// ];
 
 // {
 //     model: "Honda",
@@ -145,7 +145,7 @@ const cars = [
 //   }
 
 // I ВАРІАНТ
-const container = document.querySelector('.js-cars');
+// const container = document.querySelector('.js-cars');
 // cars.forEach(({img, model, price,type}) => {
 // const li = document.createElement('li');
 // const image = document.createElement('img');
@@ -189,15 +189,15 @@ const container = document.querySelector('.js-cars');
 
 
 // II ВАРІАНТ
-const markup = cars.map(({id,img, model, price, type})=> 
-`<li data-car-id=${id}><img src="${img}" alt="${model}" class='car-image'>
-<h2>${model}</h2>
-<h3>${type}</h3>
-<p>${price}</p>
-</li>`).join('');
+// const markup = cars.map(({id,img, model, price, type})=> 
+// `<li data-car-id=${id}><img src="${img}" alt="${model}" class='car-image'>
+// <h2>${model}</h2>
+// <h3>${type}</h3>
+// <p>${price}</p>
+// </li>`).join('');
 // console.log(markup);
 // container.insertAdjacentHTML("beforeend", markup);
-container.innerHTML = markup
+// container.innerHTML = markup
 // insertAdjacentHTML використовуємо при шаблонному рядку!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
@@ -222,3 +222,5 @@ container.innerHTML = markup
 // insertAdjacentHTML коли до старого потрібно додати нове
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

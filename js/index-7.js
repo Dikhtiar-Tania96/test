a2873d1de603723fb1279ac02b5a48c137e1f296
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-02-09 20:05:49 +0200

    add new info

--- js/index-7.js
+++ js/index-7.js
@@ -95,3 +95,15 @@
 // `);
 //   instance.show();
 // }
+
+
+const content = document.querySelector('.js-content');
+function createMarkup(){
+  let markup = '';
+
+  for(let i = 0; i < 9; i +=1){
+    markup += `<div class='item'></div>`
+  }
+
+  content.innerHTML()
+}
